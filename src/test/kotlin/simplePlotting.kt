0ecb23928497141c67d003a40bf977f9868b19db
Anatoly Nikitin <Anatoly.Nikitin@jetbrains.com>
2020-07-02 14:59:13 +0300

    Add one more check for `plotY` test

--- src/test/kotlin/simplePlotting.kt
+++ src/test/kotlin/simplePlotting.kt
@@ -1,4 +1,6 @@
 import io.kotlintest.shouldBe
+import org.jetbrains.kotlin.letsPlot.invoke
+import org.jetbrains.kotlin.letsPlot.plotLine
 import org.jetbrains.kotlin.letsPlot.plotY
 import org.junit.Test
 
@@ -7,7 +9,15 @@ class simplePlotting{
     @Test
     fun `plotY`(){
         val spec1 = (0..10).plotY { x * x }
+
         val spec2 = (0..10).plotY { it*it }
+
+        val spec3 = (0..10).plotLine {
+            x { it }
+            y { it * it }
+        }
+
         spec1 shouldBe spec2
+        spec1 shouldBe spec3
     }
 }
