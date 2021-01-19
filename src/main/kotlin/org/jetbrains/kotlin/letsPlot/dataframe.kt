e8495221b9d99d77fc35b6070db9402cdd106988
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2021-01-19 05:00:10 +0300

    Update dataframe

--- src/main/kotlin/org/jetbrains/kotlin/letsPlot/dataframe.kt
+++ src/main/kotlin/org/jetbrains/kotlin/letsPlot/dataframe.kt
@@ -7,17 +7,17 @@ import jetbrains.letsPlot.lets_plot
 import org.jetbrains.dataframe.api.columns.ColumnData
 import org.jetbrains.dataframe.impl.trackColumnAccess
 
-fun <T> PlotBuilder<*>.line(data: DataFrame<T>, body: LinesLayer<DataRow<T>>.() -> Unit) = line(data.rows, body)
-fun <T> PlotBuilder<*>.bars(data: DataFrame<T>, body: BarsLayer<DataRow<T>>.() -> Unit) = bars(data.rows, body)
-fun <T> PlotBuilder<*>.vlines(data: DataFrame<T>, body: VLinesLayer<DataRow<T>>.() -> Unit) = vlines(data.rows, body)
-fun <T> PlotBuilder<*>.hlines(data: DataFrame<T>, body: HLinesLayer<DataRow<T>>.() -> Unit) = hlines(data.rows, body)
-fun <T> PlotBuilder<*>.histogram(data: DataFrame<T>, body: HistogramLayer<DataRow<T>>.() -> Unit) = histogram(data.rows, body)
-fun <T> PlotBuilder<*>.area(data: DataFrame<T>, body: AreaLayer<DataRow<T>>.() -> Unit) = area(data.rows, body)
-fun <T> PlotBuilder<*>.density(data: DataFrame<T>, body: DensityLayer<DataRow<T>>.() -> Unit) = density(data.rows, body)
-fun <T> DataFrame<T>.plot(body: PlotBuilder<DataRow<T>>.() -> Unit) = rows.plot(DataFrameNameProvider(), body)
+fun <T> PlotBuilder<*>.line(data: DataFrame<T>, body: LinesLayer<DataRow<T>>.() -> Unit) = line(data.rows(), body)
+fun <T> PlotBuilder<*>.bars(data: DataFrame<T>, body: BarsLayer<DataRow<T>>.() -> Unit) = bars(data.rows(), body)
+fun <T> PlotBuilder<*>.vlines(data: DataFrame<T>, body: VLinesLayer<DataRow<T>>.() -> Unit) = vlines(data.rows(), body)
+fun <T> PlotBuilder<*>.hlines(data: DataFrame<T>, body: HLinesLayer<DataRow<T>>.() -> Unit) = hlines(data.rows(), body)
+fun <T> PlotBuilder<*>.histogram(data: DataFrame<T>, body: HistogramLayer<DataRow<T>>.() -> Unit) = histogram(data.rows(), body)
+fun <T> PlotBuilder<*>.area(data: DataFrame<T>, body: AreaLayer<DataRow<T>>.() -> Unit) = area(data.rows(), body)
+fun <T> PlotBuilder<*>.density(data: DataFrame<T>, body: DensityLayer<DataRow<T>>.() -> Unit) = density(data.rows(), body)
+fun <T> DataFrame<T>.plot(body: PlotBuilder<DataRow<T>>.() -> Unit) = rows().plot(DataFrameNameProvider(), body)
 
-fun <T:Number> ColumnData<T>.histogram() = lets_plot(mapOf(name to this.toList())) + geom_histogram()
-fun <T:Number> ColumnData<T>.bars() = lets_plot(mapOf(name to this.toList())) + geom_bar { x = name }
+fun <T:Number> ColumnData<T>.histogram() = lets_plot(mapOf(name() to this.toList())) + geom_histogram()
+fun <T:Number> ColumnData<T>.bars() = lets_plot(mapOf(name() to this.toList())) + geom_bar { x = name() }
 
 fun <T> DataFrame<T>.plotBars(body: BarsLayer<DataRow<T>>.() -> Unit) = plot {
     bars(body)
